f698644033c9a7fc9c3b815288bc2fda440965c2
Hans <hansz_2000@hotmail.com>
2016-09-06 20:19:08 -0400

    .

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ module.exports = new Script({
     },
 
     askName: {
-        prompt: (bot) => bot.say('Before we begin, tell me a little about yourself.\n What\'s your name?'),
+        prompt: (bot) => bot.say('Before we begin, tell me a little about yourself. What\'s your name?'),
         receive: (bot, message) => {
             const name = message.text;
             return bot.setProp('name', name)
