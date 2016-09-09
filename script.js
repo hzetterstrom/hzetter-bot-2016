1df172ce788b64e5093881681521b2c20c4e954a
hzetterstrom <hansz_2000@hotmail.com>
2016-09-09 12:36:44 -0400

    .

--- script.js
+++ script.js
@@ -83,15 +83,14 @@ menuNew: {
 		
 				//prompt: (bot) => bot.say(`OK!\n Let\'s get down to work. ` +
 				
+		prompt: (bot,name) => bot.say(`%[Contact](reply:contact) %[Analogy](reply:analogy) %[Tweet](reply:tweet)`), 
+			
+		receive: (bot, message) => {
+        var menu = message.text;
+        lmenu = menu.toLowerCase();
 		
-				prompt: (bot,name) => bot.say(`Let me know what information you\'d like to see. ` +
-				`%[Contact Hans](reply:Contact) %[Random Hans Analogy](reply:Analogy) %[Hans\' Latest Tweet](reply:Tweet)`), 
-				
-				
-				receive: (bot, message) => {
-				var menu = message.text;
-				var lmenu = menu.toLowerCase();
-				
+			//	prompt: (bot,name) => bot.say(`Let me know what information you\'d like to see. ` +
+				//`%[Contact Hans](reply:Contact) %[Random Hans Analogy](reply:Analogy) %[Hans\' Latest Tweet](reply:Tweet)`), 
 			
 			if ( lmenu.indexOf('contact') >= 0){
 				return bot.say(`%[Import Hans\' contact info](http://en.gravatar.com/6df718bd56665a8d924fb58f3c23278b.vcf) %[Hans\' Gravatar Profile](https://en.gravatar.com/6df718bd56665a8d924fb58f3c23278b)`)
