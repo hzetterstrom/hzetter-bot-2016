cda4e4cd6c394feb12d79cf0ef4af91e7d42c951
hzetterstrom <hansz_2000@hotmail.com>
2016-09-12 20:18:33 -0400

    .

--- script.js
+++ script.js
@@ -133,7 +133,7 @@ menuNew: {
 			//	.then(() => bot.say('Why not tweet @HansZed and let him know we\'re talking? Just click this link and I\'ll create a tweet for you.' + '%[Tweet Hans](https://twitter.com/intent/tweet?text=Chatbots%20are%20taking%20over.%20James%20-%20Virtual%20Assistant%20to%20%40HansZed%20and%20I%20are%20chatting...)')
 				.then(() => bot.say('Why not tweet @HansZed and let him know we\'re talking? Just click this link and I\'ll create a tweet for you.' + `%[Tweet Hans](https://twitter.com/intent/tweet\?text=Chatbots\%20are\%20taking\%20over\.\%20James\%20\-\%20Virtual\%20Assistant\%20to\%20\%40HansZed\%20and\%20I\%20are\%20chatting\.\.\.)`))
 				//.then(() => bot.say('Or you can Follow @HansZed by clicking the button/link' + `%[Follow Hans](https://twitter.com/intent/follow\?screen\_name\=hanszed`))
-				.then(() => bot.say(`%[Follow Hans](https://twitter.com/intent/follow\?screen\_name=hanszed)`))
+				.then(() => bot.say('Or you can follow @HansZed by clicking on the link/button' + `%[Follow Hans](https://twitter.com/intent/follow\?screen\_name=hanszed)`))
 				} //  end inner for loop
 			}); // end call to feed (feed-read) method
  } // end urls for loop
