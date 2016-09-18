0ee8e6db2b17345d1d2593e2afd67c04771e2596
hzetterstrom <hansz_2000@hotmail.com>
2016-09-18 11:58:07 -0400

    .

--- script.js
+++ script.js
@@ -77,7 +77,8 @@ menuNew: {
         var lmenu = menu.toLowerCase();
 			
 			if ( lmenu.indexOf('info') >= 0){
-			return('info');
+			bot.say('What more can I tell you about Hans?' + `%[Email Hans](reply:email) %[Text Hans](reply:text) %[Get Hans\' contact info](http://goo.gl/V2CrZR)` + '\nOr menu to return to the main menu'),
+			.then(() => 'info');
 //			prompt: (bot,name) => bot.say('What more can I tell you about Hans?' + `%[Email Hans](reply:email) %[Text Hans](reply:text) %[Get Hans\' contact info](http://goo.gl/V2CrZR)` + '\nOr menu to return to the main menu'),
 	//			receive: (bot, message2) => {
 		//	var innermenu = message2.text;
@@ -167,7 +168,7 @@ nextMenu:{
 },
 
 info: {
-prompt: (bot,name) => bot.say('What more can I tell you about Hans?' + `%[Email Hans](reply:email) %[Text Hans](reply:text) %[Get Hans\' contact info](http://goo.gl/V2CrZR)` + '\nOr menu to return to the main menu'),
+
 			receive: (bot, message2) => {
 			var innermenu = message2.text;
 			var linnermenu = innermenu.toLowerCase();
