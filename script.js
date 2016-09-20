b4d3d7c934e95706015c017c4783a5426d561902
hzetterstrom <hansz_2000@hotmail.com>
2016-09-20 09:59:09 -0400

    .

--- script.js
+++ script.js
@@ -106,7 +106,7 @@ work: {
 				
 				
 			} else if ( lmenu.indexOf('resume') >= 0){
-			return bot.say(`%[Hans\' resume](https://goo.gl/8mBhae)`)
+			return bot.say(`%[Hans\' resume](https://goo.gl/x5fqqo)`)
 			.then(() => 'work');		
 			} else if ( lmenu.indexOf('menu') >= 0){
 				return('menuTop');
