3f24a7eaff2fbc27e754c2d74f414509dd535f52
hzetterstrom <hansz_2000@hotmail.com>
2016-09-25 11:32:12 -0400

    .

--- script.js
+++ script.js
@@ -175,9 +175,9 @@ work: {
 			.then(() => 'work');
 			} else if ( lmenu.indexOf('experience') >= 0){
 			return bot.say(`Hans\' latest experiences include:\n`)	
-			.then(() => bot.say(`•	Designed and implemented organization-wide Kanban room that visually represents impact of decision-making, team collaboration, and performance against business and product KPIs.\n`))
-			.then(() => bot.say(`•	Led the change in SDLC methodologies from Waterfall to Agile, and supported process and culture to create a team devoted to the spirit of the Agile Manifesto.\n`))
-			.then(() => bot.say(`•	Reorganized company structure to create Product Managers that better represent product lines using business model canvases, product roadmaps, qualitative research methods, and KPI management that resulted in more autonomy and a customer-centered focus.\n`))
+			.then(() => bot.say(`•	Created Kanban room to visually represent organizational decisions and KPI performance. \n`))
+			.then(() => bot.say(`•	Implemented Agile SDLC methodologies  and initiated a team culture devoted to the Agile Manifesto.\n`))
+			//.then(() => bot.say(`•	Reorganized company structure to create Product Managers using business model canvases, product roadmaps, qualitative research methods, and KPI management that resulted in more autonomy and a customer-centered focus.\n`))
 			.then(() => 'work');
 				
 				
@@ -261,8 +261,8 @@ thoughts: {
 	
 	
 				// loop through our list of RSS feed urls
-				for (var j = 0; j < urls.length; j++) {
-
+				//for (var j = 0; j < urls.length; j++) {
+				for (var j = 0; j < 3; j++) {
 				// fetch rss feed for the url:
 				feed(urls[j], function(err, articles) {
 
