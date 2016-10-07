8d420af895ae952f8e9137f5bb0851d9697300df
hzetterstrom <hansz@Hanss-MacBook-Air.local>
2016-10-07 12:48:38 -0400

    .

--- script.js
+++ script.js
@@ -39,11 +39,11 @@ module.exports = new Script({
     askName: {
         prompt: (bot) => bot.say('Before we begin, tell me a little about yourself. What\'s your name?'),
         receive: (bot, message) => {
-            const tname = message.text;
-            var tname2 = tname.toLowerCase();
-            var tname3 = tname2.replace("my name is", "");
-            tname3 = tname2.replace("I\'m called", "");
-            return bot.setProp('tname3', name)
+            const name = message.text;
+            name = name.toLowerCase();
+            name = name.replace("my name is", "");
+            name = name.replace("I\'m called", "");
+            return bot.setProp('name', name)
             .then(() => bot.say('It\'s nice to meet you, ' + name + '.'))
                 .then(() => 'Reason');
         }
