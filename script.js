7dcce8e8c0ca0aa5f7ca5c8b163631f9302c98b5
hzetterstrom <hansz@Hanss-MacBook-Air.local>
2016-10-07 09:09:15 -0400

    .

--- script.js
+++ script.js
@@ -305,13 +305,13 @@ thoughts: {
 			}); // end call to feed (feed-read) method
 			var promise = new Promise(function(resolve, reject) {
 			//setTimeout(function () {
-			var smooch = require('smooch')	
+			var Smooch = require('smooch')	
 			Smooch.init({appToken: '7rmh6j05pm8dtjff9atjnl9lx'});
 			//smooch.appUsers.sendMessage('c7f6e6d6c3a637261bd9656f', {
-			//smooch.appUsers.sendMessage(userId, {
-			//text: 'Just put some vinegar on it',
-			//role: 'appUser'
-			//})
+			smooch.appUsers.sendMessage(Smooch.userId, {
+			text: 'Just put some vinegar on it',
+			role: 'appUser'
+			})
 //			console.log("carousel")
 //		var smooch = require('smooch')	
 //		smooch.appUsers.sendMessage('7rmh6j05pm8dtjff9atjnl9lx', {
